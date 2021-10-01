f181fc746b67aa0ccbefbb9a06c6e8226c31893c
wogit <vilathangarajavila@yahoo.com>
2021-10-01 13:17:05 -0400

    Update Test.java
    Added system.out.

--- Test.java
+++ Test.java
@@ -4,8 +4,7 @@
 public class Test{
   public static void main (String arg[]){
     System.out.println("Test is working");
-    
-    
+    System.out.println("Test is working again");
     // Test Method
   }
 
